c35c379aa7b07623b8d589eb22ead2d31250a219
Serge <sergeykonstwork@gmail.com>
2021-11-19 13:10:34 +0300

    Реализована очередь на массиве и сортировка очереди Произведён подсчёт О-Символики. Первая версия

--- src/mainpart/MergeSort.java
+++ src/mainpart/MergeSort.java
@@ -15,23 +15,29 @@ public class MergeSort {
         int step = 1; //+1 // Шаг разбиения последовательности. Изначально равен единице.
         int[] temp = new int[size];//+1
         int left = 0;//+1
+        Main.nOp+=3;
 
         //Пока шаг меньше длины массива
        // ∑(step=1 -> n/2)(1 + 1 + 1 + 2 + 3 + 20.5n^3+71.5n^2+113n+62 + 4n^2+20n+18) = ∑(step=1 -> n/2)(88 + 20.5n^3 + 75.5n^2 + 133n) = 20.5n^3(n/2) + 75.5n^2(n/2) + 133n(n/2) + 88(n/2) = 10.25n^4+37.75n^3+66.5n^2+44n;
         while (step < size) { //+1
             int index = 0;//+1
             left = 0;//+1
+            Main.nOp+=3;
             int midl = left + step; //+2
             int right = left + step * 2; //+3
+            Main.nOp+=5;
             //∑(left=0 -> n)(1+3+3+1+1+6n^2+34n+44+4n^2+17n+10.5n^2+9)=∑(left=0 -> n)(62+51n+20.5n^2)= 20.5n^2(n+1) + 51n(n+1) + 62(n+1) = 20.5n^3+71.5n^2+113n+62;
             while( left < size ) {//+1
+                Main.nOp++;
                 midl = Math.min(midl, size); //+3 //// сортируемый участок не выходит за границы последовательности
                 right = Math.min(right, size);//+3
                 int i1 = left;//+1
                 int i2 = midl;//+1
+                Main.nOp+=8;
                 //В худшем случае midl = n/2
                 //∑(i=0 -> n/2)(12n+44)= 12n∑(i=1 -> n/2+1)(1) + 44∑(i=1 -> n/2+1)(1) = 12n(n/2+1) + 44(n/2+1) = 6n^2+34n+44;
                 while (i1 < midl && i2 < right) { //+3
+                    Main.nOp+=3;
                     if (queue.get(i1) < queue.get(i2)) {//8n+25 в худшем случае берётся такой мАССив, что его левая часть всегда больше правой. Следовательно выполняться будет только else
                         temp[index++] = queue.get(i1++);
                     } else{
@@ -41,22 +47,28 @@ public class MergeSort {
                 }
                 //∑(i1=0 -> n/2)(4n+17) = 4n∑(i1=1 -> n)(1) + 17∑(i1=1 -> n)(1) = 4n^2+17n;
                 while (i1 < midl){//+1
+                    Main.nOp++;
                     temp[index++] = queue.get(i1++);//4n+16
                 }
                 //∑(i2=n/2 -> n-1)(4n+17)= 4n∑(i2=n/2 -> n-1)(1) + 17∑(i2=n/2 -> n-1)(1) = 4n(n/2) + 17(n/2) = 2n^2+8.5n^2 = 10.5n^2;
                 while (i2 < right){//+1
+                    Main.nOp++;
                     temp[index++] = queue.get(i2++);//4n+16
                 }
                 left += step * 2;//+3
                 midl += step * 2;//+3
                 right += step * 2;//+3
+                Main.nOp+=9;
             }
 
             for (int i = 0; i < size; i++) {//+2+∑(i=0 -> n)(2+4n+14) = 2+4n(n+1)+16(n+1) = 4n^2+4n+16n+16+2 = 4n^2+20n+18;
+                Main.nOp+=2;
                 queue.set(i, temp[i]);//4n+14
             }
             step *= 2;//+2
+            Main.nOp+=2;
         }
+        Main.nOp++;
         return queue; //+1
     }
 }
--- src/mainpart/Queue.java
+++ src/mainpart/Queue.java
@@ -14,6 +14,7 @@ public class Queue {
     //Метода push: 1 + 1 = 2 --- O(2)=O(1)=1
     public void push(int item){ //+1 (Так как метод принимает в себя значение.) Можно посчитать потом, когда отдаю сам элемент в сортировке
         queue.add(item); //+1, так как передаётся для добавления (item))
+        Main.nOp +=2;
     }
 
     //Попсня. Берём первое число в очереди, возвращаем его и удаляем из очереди.
@@ -21,10 +22,12 @@ public class Queue {
     public int pop(){
         int first = queue.get(0);//+2
         queue.remove(0);//+1
+        Main.nOp+=4;
         return first; //+1
     }
     //O(1)=1
     public int showFirst(){
+        Main.nOp+=2;
         return queue.get(0);//+2
     }
 
@@ -32,29 +35,35 @@ public class Queue {
 
     //+1+4n+2+2+6+1=4n+12=f(n) -- O(f(n))=n
     public int get(int position){//+1
-
+        Main.nOp++;
         for (int i = 0; i < position; i++){ //+2+∑(i=0 -> n-1)(+2+2)=+2+4*∑(i=1 -> n)(1)=+2+4*(n)=4n+2
             push(pop());//+2
+            Main.nOp+=2;
         }
 
         int first = showFirst();//+2
+        Main.nOp+=2;
         //В худшем случае выполниться один раз цикл
         for (int i = position; i < queue.size(); i++){//+2+∑(i=n-1 -> n)(+4)=2+4*∑(i=n-1 -> n)(1)=6
             push(pop());//+2
+            Main.nOp+=2;
         }
+        Main.nOp++;
         return first; //+1
     }
 
     //+2+4n+2+2+6+1=4n+13=f(n) -- O(f(n))=n
     public void set(int position, int value){
-
+        Main.nOp+=2;
         for (int i = 0; i < position; i++){
             push(pop());
+            Main.nOp+=2;
         }
 
         queue.set(0,value);
         for (int i = position; i < queue.size(); i++) {
             push(pop());
+            Main.nOp+=2;
         }
     }
 
